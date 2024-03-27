2b916e0d956ba5187976e9544b5860feecdf8780
soft-ly <157381984+soft-ly@users.noreply.github.com>
2024-03-27 16:39:15 +0900

    update!

---
+++ Learning/if4.py
@@ -0,0 +1,12 @@
+a = int(input("첫 숫자"))
+b = int(input("두번째 숫자"))
+c = input("무슨 계산?")
+
+if c=="+":
+    print(a+b)
+elif c == "-":
+    print(a-b)
+elif c == "*":
+    print(a*b)
+elif c == "/":
+    print(a/b)
